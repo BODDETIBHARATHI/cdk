6a972bbc93d3d1c2bd01549c5f7a03dbaf1666a3
BODDETIBHARATHI <52271275+BODDETIBHARATHI@users.noreply.github.com>
2020-08-26 06:25:38 +0530

    Create Shape.java

---
+++ Core Java/Day2 Assignments/Interface/Shape/Shape.java
@@ -0,0 +1,18 @@
+
+public class Shape1 {
+       private String name;
+       public Shape1() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Shape1(String name) {
+		super();
+		this.name = name;
+	}
+       
+}
